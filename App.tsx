ab4527d1ca063366890171ebdfc38299e8c1ad52
osdnk <micosa97@gmail.com>
2023-10-09 23:20:40 +0300

    copy basic example in

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
-import { View, useWindowDimensions,Text } from 'react-native';
+import { View, useWindowDimensions,Text, SafeAreaView, StyleSheet } from 'react-native';
 import { TabView, SceneMap } from 'react-native-tab-view';
+import { BasicPagerViewExample } from "./BasicPagerViewExample";
 
 const FirstRoute = () => (
   <View style={{ flex: 1, backgroundColor: '#ff4081' }} >
@@ -29,12 +30,15 @@ const App = () => {
   ]);
 
   return (
-    <TabView
-      navigationState={{ index, routes }}
-      renderScene={renderScene}
-      onIndexChange={setIndex}
-      initialLayout={{ width: layout.width }}
-    />
+    <SafeAreaView style={StyleSheet.absoluteFill}>
+      {/*<TabView*/}
+      {/*  navigationState={{ index, routes }}*/}
+      {/*  renderScene={renderScene}*/}
+      {/*  onIndexChange={setIndex}*/}
+      {/*  initialLayout={{ width: layout.width }}*/}
+      {/*/>*/}
+      <BasicPagerViewExample/>
+    </SafeAreaView>
   );
 }
 
